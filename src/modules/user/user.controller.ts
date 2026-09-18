7cf3a16ebc45ec82e1a6f83d5823cb6a78e5c1d9
Akash Sarker <akashsarker2478@gmail.com>
2026-09-18 22:06:00 +0600

    refactor: extract sendResponse helper to separate file

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,31 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
-type TMeta = {
-  page:number;
-  limit:number;
-  total:number;
-}
 
-type TResponseData <T> = {
-  success :boolean;
-  statusCode: number;
-  message:string;
-  data:T;
-  meta ?:TMeta
-}
-
-const sendResponse = <T>(res:Response,data:TResponseData<T>)=>{
-  res.status(data.statusCode).json({
-    success:data.success,
-    statusCode : data.statusCode,
-    message:data.message,
-    data:data.data,
-    meta:data.meta
-  })
-}
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
